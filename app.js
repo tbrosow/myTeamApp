a61d137701a157a715d14d28090aeae18a4a4f10
Torsten Brosow <torsten.brosow@gmail.com>
2018-11-16 06:57:24 +1100

    test

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ app.use(session({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 app.use(express.static('/Users/Torsten/Programs/nodesql/auth/authenticationIntro'));
 
 // serve static files from template
